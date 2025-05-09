b2dc26e0103e8075cdb42340ff7481b40637c2ec
KirinB <lee.minhnhan29@gmail.com>
2025-05-09 18:09:02 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "dathangsieuthi.com/demo",
-    port: 80,
+    host: "https://dathangsieuthi.com/demo",
+    port: 443,
   },
 });
